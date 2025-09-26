da67dd12bc85495aea2dc2c1b0a1e633dd9ec330
Wa-ilhi <rlynebanks@gmail.com>
2025-09-26 16:04:21 +0800

    changes on App

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
   <div id="app">
     <router-view />
-    <InstallButton />
   </div>
 </template>
-
-<script>
-import InstallButton from './components/InstallButton.vue'
-
-export default {
-  components: { InstallButton }
-}
-</script>
